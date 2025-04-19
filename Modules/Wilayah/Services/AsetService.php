5a8b9369b3d6dee83d1d39f9d2809928a2cc878d
ernuyoga <ernuyoganirwana@gmail.com>
2025-04-19 20:10:45 +0700

    menambahkan fungsi service untuk penanganan upload foto pada aset

--- Modules/Wilayah/Services/AsetService.php
+++ Modules/Wilayah/Services/AsetService.php
@@ -5,6 +5,8 @@ namespace Modules\Wilayah\Services;
 use Modules\Wilayah\Entities\Aset;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
 
 class AsetService
 {
@@ -27,21 +29,90 @@ class AsetService
     public function create(array $data): Aset
     {
         return DB::transaction(function () use ($data) {
-            return Aset::create($data);
+            $fotoFiles = $data['fotos'] ?? [];
+            unset($data['fotos']);
+
+            $aset = Aset::create($data);
+
+            if (!empty($fotoFiles)) {
+                foreach ($fotoFiles as $file) {
+                    if ($file instanceof UploadedFile) {
+                        $instansiId = $data['instansi_id'];
+                        $filename = $data['warga_id'] . '_' . time() . '_' . $file->getClientOriginalName();
+                        $path = $file->storeAs("aset_foto/{$instansiId}", $filename, 'public');
+
+                        $aset->fotos()->create([
+                            'nama' => $file->getClientOriginalName(),
+                            'file_path' => "aset_foto/{$instansiId}/{$filename}",
+                        ]);
+                    }
+                }
+            }
+
+            return $aset->load(['fotos']);
         });
     }
 
     public function update(Aset $aset, array $data): Aset
     {
-        DB::transaction(function () use ($aset, $data) {
-            $aset->update($data);
-        });
+        return DB::transaction(function () use ($aset, $data) {
+            $fotoBaru = $data['fotos'] ?? [];
+            $hapusFotoIds = $data['hapus_foto'] ?? [];
 
-        return $aset;
+            unset($data['fotos'], $data['hapus_foto']);
+
+            $aset->update($data);
+
+            if (!empty($hapusFotoIds)) {
+                $fotos = $aset->fotos()->whereIn('id', $hapusFotoIds)->get();
+
+                foreach ($fotos as $foto) {
+                    if (Storage::disk('public')->exists($foto->file_path)) {
+                        Storage::disk('public')->delete($foto->file_path);
+                    }
+
+                    $foto->delete();
+                }
+            }
+
+            if (!empty($fotoBaru)) {
+                foreach ($fotoBaru as $file) {
+                    if ($file instanceof UploadedFile) {
+                        $instansiId = $aset->instansi_id;
+                        $filename = $aset->warga_id . '_' . time() . '_' . $file->getClientOriginalName();
+                        $path = $file->storeAs("aset_foto/{$instansiId}", $filename, 'public');
+
+                        $aset->fotos()->create([
+                            'nama' => $file->getClientOriginalName(),
+                            'file_path' => "aset_foto/{$instansiId}/{$filename}",
+                        ]);
+                    }
+                }
+            }
+
+            return $aset->load(['fotos']);
+        });
     }
 
     public function delete(Aset $aset): bool|null
     {
         return $aset->delete();
+    }
+
+    public function destroy(Aset $aset): bool
+    {
+        return DB::transaction(function () use ($aset) {
+            $fotos = $aset->fotos;
+
+            foreach ($fotos as $foto) {
+                if (Storage::disk('public')->exists($foto->file_path)) {
+                    Storage::disk('public')->delete($foto->file_path);
+                }
+
+                $foto->delete();
+            }
+
+            return $aset->delete();
+        });
     }
 }
