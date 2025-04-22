940df0e2d71ff257b3d085baf590b66475ccb618
Orvin Kent <kent.orvin@gmail.com>
2025-04-22 11:19:09 +0700

    edit current seeders

--- Modules/Wilayah/Database/Seeders/WilayahDatabaseSeeder.php
+++ Modules/Wilayah/Database/Seeders/WilayahDatabaseSeeder.php
@@ -29,7 +29,7 @@ class WilayahDatabaseSeeder extends Seeder
                 'id' => 2,
                 'warga_id' => 2,
                 'instansi_id' => 2,
-                'aset_m_jenis_id' => 2,
+                'aset_m_jenis_id' => 1,
                 'nama' => 'Toko Madura Bu Herman',
                 'alamat' => 'Jl. Mertojoyo No. 11, RT 15, RT 05, Kelurahan Merjosari, Kecamatan Lowokwaru, Kota Malang',
                 'lokasi' => '-7.944033233173902, 112.6023300015707'
@@ -38,7 +38,7 @@ class WilayahDatabaseSeeder extends Seeder
                 'id' => 3,
                 'warga_id' => 3,
                 'instansi_id' => 3,
-                'aset_m_jenis_id' => 3,
+                'aset_m_jenis_id' => 1,
                 'nama' => 'Rumah Pak Eko',
                 'alamat' => 'Jl. Mertojoyo Selatan No. 15, RT 05, RW 03, Kelurahan Merjosari, Kecamatan Lowokwaru, Kota Malang',
                 'lokasi' => '-7.944767029702089, 112.6039211320968'
@@ -47,7 +47,7 @@ class WilayahDatabaseSeeder extends Seeder
                 'id' => 4,
                 'warga_id' => 4,
                 'instansi_id' => 4,
-                'aset_m_jenis_id' => 4,
+                'aset_m_jenis_id' => 1,
                 'nama' => 'Rumah Pak Masyur',
                 'alamat' => 'Jl. Joyo Utomo No. 23, RT 15, RW 05, Kelurahan Merjosari, Kecamatan Lowokwaru, Kota Malang',
                 'lokasi' => '-7.943403190792656, 112.60346421014522'
@@ -56,7 +56,7 @@ class WilayahDatabaseSeeder extends Seeder
                 'id' => 5,
                 'warga_id' => 5,
                 'instansi_id' => 5,
-                'aset_m_jenis_id' => 5,
+                'aset_m_jenis_id' => 1,
                 'nama' => 'Nasi Pecel Bu Anggi',
                 'alamat' => 'Jl. Mertojoyo Selatan No. 35, RT 15, RW 05, Kelurahan Merjosari, Kecamatan Lowokwaru, Kota Malang',
                 'lokasi' => '-7.944015476049777, 112.60395652495028'
--- Modules/Wilayah/Database/Seeders/WilayahFotoSeeder.php
+++ Modules/Wilayah/Database/Seeders/WilayahFotoSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\Umkm\Database\Seeders;
+namespace Modules\Wilayah\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
