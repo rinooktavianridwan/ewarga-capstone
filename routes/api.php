dacbecfa8f36a7f48d805ecdc869f9888963a830
Rino <rinooktavianridwan@gmail.com>
2025-04-24 11:27:49 +0700

    menghapus autentifikasi

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,3 @@ use App\Http\Controllers\AuthController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
