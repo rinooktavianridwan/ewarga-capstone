c68a197f71a1dd531320ad291c89fd83d32c19bb
ernuyoga <ernuyoganirwana@gmail.com>
2025-05-01 20:39:40 +0700

    mengubah kode route api untuk update

--- Modules/Umkm/Routes/api.php
+++ Modules/Umkm/Routes/api.php
@@ -14,14 +14,14 @@ Route::middleware('auth:sanctum')->prefix('umkm')->group(function () {
     Route::get('/', [PendataanUmkmController::class, 'index']);
     Route::get('/{umkm}', [PendataanUmkmController::class, 'show']);
     Route::post('/', [PendataanUmkmController::class, 'store']);
-    Route::post('/{umkm}', [PendataanUmkmController::class, 'update']);
+    Route::put('/{umkm}', [PendataanUmkmController::class, 'update']);
     Route::delete('/{umkm}', [PendataanUmkmController::class, 'destroy']);
 
     Route::prefix('produk')->group(function () {
         Route::get('/search', [PendataanProdukController::class, 'index']);
         Route::get('/{produk}', [PendataanProdukController::class, 'show']);
         Route::post('/store', [PendataanProdukController::class, 'store']);
-        Route::post('/{produk}', [PendataanProdukController::class, 'update']);
+        Route::put('/{produk}', [PendataanProdukController::class, 'update']);
         Route::delete('/{produk}', [PendataanProdukController::class, 'destroy']);
     });
 
