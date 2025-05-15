06bed2bc9db5d90a25b7f0d7242e86c545e44915
Rino <rinooktavianridwan@gmail.com>
2025-05-15 11:22:50 +0700

    memperbaiki exception login

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\InstansiController;
 |
 */
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
